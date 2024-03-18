f0c19529efdbb936a76fadde69e13f5592793729
wander <wander@ffactory.org>
2024-03-18 16:43:13 +0800

    fix bug

--- README.md
+++ README.md
@@ -4,26 +4,29 @@ bson DateTime 转json 帮助函数，转换指定时区和格式的时间格式
 # Example 
 
 ```
-use serialize_with_bson::{datetime_to_tz, datetime_to_tz_map, layout::DEFAULT, object_to_hex};
+use serialize_with_bson::{
+    datetime_to_tz, datetime_to_tz_map, layout::DEFAULT, object_to_hex, time_zone_and_layout,
+    TimeZoner,
+};
 use bson::{doc, oid::ObjectId, DateTime, Document};
 use serde::{Deserialize, Serialize};
 use serde_json::to_string;
 use std::{collections::HashMap, str::FromStr};
  
-    time_zone_and_layout!("Asia/Tokyo", DEFAULT); // 给 bson::DateTime 设定时区和时间字符串输出格式
+    time_zone_and_layout!("Asia/Tokyo", DEFAULT,BsonDateTime); // 设定 DateTime的包装类型为BsonDateTime 设定时区和时间字符串输出格式
 
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
     pub struct Bacterium {
         #[serde(rename = "_id", serialize_with = "object_to_hex")] // 启用自定义序列化函数 object_to_hex 把ObjectID转成hex字符串
         pub id: ObjectId,
         pub has_genome: bool,
-        #[serde(serialize_with = "datetime_to_tz")] // 启用自定义序列化函数datetime_to_tz 把DateTime转到指定时区时间
-        pub creation_time: DateTime,
+        #[serde(serialize_with = "datetime_to_tz")] // 启用自定义序列化函数datetime_to_tz 把BsonDateTime转到指定时区时间
+        pub creation_time: BsonDateTime,
         #[serde(serialize_with = "datetime_to_tz")]
-        pub modified_time: DateTime,
+        pub modified_time: BsonDateTime,
         pub short_id: String,
-        #[serde(serialize_with = "datetime_to_tz_map")] // 启用自定义序列化函数datetime_to_tz_map 把Map中的DateTime转到指定时区时间
-        pub locations: HashMap<String, DateTime>,
+        #[serde(serialize_with = "datetime_to_tz_map")] // 启用自定义序列化函数datetime_to_tz_map 把Map中的BsonDateTime转到指定时区时间
+        pub locations: HashMap<String, BsonDateTime>,
         pub taxonomy: Document,
         pub backtrace: Vec<String>,
     }
@@ -33,22 +36,22 @@ fn main() {
         let mut map = HashMap::new();
         map.insert(
             "R3R-A-9-2-L5".to_string(),
-            DateTime::from_millis(1571985978429),
+            DateTime::from_millis(1571985978429).into(),
         );
         map.insert(
             "R3T-A-5-3-K6".to_string(),
-            DateTime::from_millis(1571984742668),
+            DateTime::from_millis(1571984742668).into(),
         );
         map.insert(
             "R3R-A-10-3-L7".to_string(),
-            DateTime::from_millis(1571985444876),
+            DateTime::from_millis(1571985444876).into(),
         );
 
         let src = Bacterium {
             id: ObjectId::from_str("5db131829181e500010b93d6").unwrap(),
             has_genome: false,
-            creation_time: DateTime::from_millis(1571893634109),
-            modified_time: DateTime::from_millis(1690439186944),
+            creation_time: DateTime::from_millis(1571893634109).into(),
+            modified_time: DateTime::from_millis(1690439186944).into(),
             short_id: "B1DXX".to_owned(),
             locations: map,
             taxonomy: doc! {
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 pub mod layout;
 use bson::oid::ObjectId;
+use bson::DateTime;
 use gostd_time::{LoadLocation, UnixMilli};
 use serde::ser::{SerializeMap, Serializer};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 /// 指定时区特征
@@ -65,70 +66,89 @@ where
 /// 设置时区和输出日期时间输出格式的宏
 #[macro_export]
 macro_rules! time_zone_and_layout {
-    ( $( $x:tt,$y:expr )* ) => {
-        $(
-            impl crate::TimeZoner for bson::DateTime {
-                fn tz_name(&self) -> &str {
-                    return $x;
-                }
-
-                fn timestamp(&self) -> i64 {
-                    self.timestamp_millis()
-                }
-                fn layout(&self) -> &str{
-                    return $y;
-                }
+    ( $tz_name:tt, $layout:ident,$new_type:ident) => {
+        #[derive(
+            Eq, PartialEq, Ord, PartialOrd, Hash, Copy, Clone, Serialize, Deserialize, Debug,
+        )]
+        pub struct $new_type(DateTime);
+        impl From<DateTime> for $new_type {
+            fn from(date: DateTime) -> Self {
+                Self(date)
             }
-)* };
+        }
+        impl $new_type {
+            pub fn timestamp_millis(&self) -> i64 {
+                self.0.timestamp_millis()
+            }
+        }
+
+        impl From<$new_type> for DateTime {
+            fn from(bson_date_time: BsonDateTime) -> Self {
+                bson_date_time.0
+            }
+        }
+        impl TimeZoner for $new_type {
+            fn tz_name(&self) -> &str {
+                return $tz_name;
+            }
+
+            fn timestamp(&self) -> i64 {
+                self.timestamp_millis()
+            }
+            fn layout(&self) -> &str {
+                return $layout;
+            }
+        }
+    };
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::{datetime_to_tz, datetime_to_tz_map, layout::DEFAULT, object_to_hex};
+    use crate::{datetime_to_tz, datetime_to_tz_map, layout::DEFAULT, object_to_hex, TimeZoner};
     use bson::{doc, oid::ObjectId, DateTime, Document};
     use serde::{Deserialize, Serialize};
     use serde_json::to_string;
-    use std::{collections::HashMap, str::FromStr};
+    use std::{collections::HashMap, convert::From, str::FromStr};
 
-    time_zone_and_layout!("Asia/Tokyo", DEFAULT);
+    time_zone_and_layout!("Asia/Tokyo", DEFAULT, BsonDateTime);
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
     pub struct Bacterium {
         #[serde(rename = "_id", serialize_with = "object_to_hex")]
         pub id: ObjectId,
         pub has_genome: bool,
         #[serde(serialize_with = "datetime_to_tz")]
-        pub creation_time: DateTime,
+        pub creation_time: BsonDateTime,
         #[serde(serialize_with = "datetime_to_tz")]
-        pub modified_time: DateTime,
+        pub modified_time: BsonDateTime,
         pub short_id: String,
         #[serde(serialize_with = "datetime_to_tz_map")]
-        pub locations: HashMap<String, DateTime>,
+        pub locations: HashMap<String, BsonDateTime>,
         pub taxonomy: Document,
         pub backtrace: Vec<String>,
     }
 
     #[test]
     fn test_convert_to_json() {
-        let mut map = HashMap::new();
+        let mut map: HashMap<String, BsonDateTime> = HashMap::new();
         map.insert(
             "R3R-A-9-2-L5".to_string(),
-            DateTime::from_millis(1571985978429),
+            DateTime::from_millis(1571985978429).into(),
         );
         map.insert(
             "R3T-A-5-3-K6".to_string(),
-            DateTime::from_millis(1571984742668),
+            DateTime::from_millis(1571984742668).into(),
         );
         map.insert(
             "R3R-A-10-3-L7".to_string(),
-            DateTime::from_millis(1571985444876),
+            DateTime::from_millis(1571985444876).into(),
         );
 
         let src = Bacterium {
             id: ObjectId::from_str("5db131829181e500010b93d6").unwrap(),
             has_genome: false,
-            creation_time: DateTime::from_millis(1571893634109),
-            modified_time: DateTime::from_millis(1690439186944),
+            creation_time: DateTime::from_millis(1571893634109).into(),
+            modified_time: DateTime::from_millis(1690439186944).into(),
             short_id: "B1DXX".to_owned(),
             locations: map,
             taxonomy: doc! {
